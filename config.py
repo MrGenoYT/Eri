637bf504545aebfac69747792f951794281f712e
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-06-21 23:47:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+TOKEN = "YOUR_DISCORD_TOKEN"
+MONGO_URI = "YOUR_MONGO_URI"
+DEFAULT_PERSONA = {"name": "Eri", "age": "17", "gender": "Female", "moods": ["Cool", "Chill"], "traits": ["Playful", "Funny"]}
